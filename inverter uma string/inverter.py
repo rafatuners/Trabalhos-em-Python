052f544181646e061d9927f3f5a403b31fa1df1f
Rafael Gagliardi Maciel da Cruz <rcruz4513@yahoo.com.br>
2023-03-02 15:25:44 -0300

    Create inverter.py

---
+++ inverter uma string/inverter.py
@@ -0,0 +1,5 @@
+palavra = str(input("Digite uma palavra: "))
+palavra = palavra.upper()
+print(f'Você digitou {palavra}')
+invertida = palavra[::-1]
+print(f'A palavra que você digitou invertida fica {invertida}')
